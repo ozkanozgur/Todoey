643a64a088edf65a00cd3dc0fd7a05747060d571
Özgür Yılmaz <o.ozguryilmaz@gmail.com>
2018-05-10 17:28:59 +0300

    initial commit

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Özgür Yılmaz on 10.05.2018.
+//  Copyright © 2018 ozkan ozgur yilmaz. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike","Buy Eggos","Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+
+}
+
