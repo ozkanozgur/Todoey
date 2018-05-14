0bb63e460ed53e34f2258bd661c90cff1b18c579
Özgür Yılmaz <o.ozguryilmaz@gmail.com>
2018-05-14 16:59:00 +0300

    todo list completed with dummy data

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -30,6 +30,23 @@ class TodoListViewController: UITableViewController {
         
         return cell
     }
+    
+    //MARK - TableView delegate method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+    }
 
 }
 
